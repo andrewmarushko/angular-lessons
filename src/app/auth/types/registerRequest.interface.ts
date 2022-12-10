25880794fdc13d05029c40592038d775631843bb
Andrew Marushko <andrewmarushko@gmail.com>
2022-12-10 01:54:47 +0100

    add reducer and actions

---
+++ src/app/auth/types/registerRequest.interface.ts
@@ -0,0 +1,5 @@
+export interface RegisterRequesrInterface {
+  email: string
+  password: string
+  username: string
+}
---
+++ src/app/shared/types/currentUser.interface.ts
@@ -0,0 +1,10 @@
+export interface CurrentUserInterface {
+  id: number
+  email: string
+  bio: string | null
+  username: string
+  image: string | null
+  token: string
+  createdAt: string
+  updatedAt: string
+}
